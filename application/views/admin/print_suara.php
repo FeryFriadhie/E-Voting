df1cf228e7c2421acc612438a4abacbf12b62c9e
FeryFriadhie <feryfriadhie@gmail.com>
2022-06-25 07:56:23 +0700

    first commit

---
+++ application/views/admin/print_suara.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.min.css">
+</head>
+<body>
+    <!-- <div class="col-md-12">
+    <img src="<?= base_url('assets/'); ?>image/smkn1ciamis.png" alt="logo smkn 1 ciamis" style="position: absolute; width:60px; height: auto;">
+    <table>
+        <tr>
+            <td align="center">
+                <span style="line-height: 1.6; font-weight: bold;">
+                    OSIS <br>
+                    SMK NEGERI 1 CIAMIS <br>
+                    Jl. jenderal Sudirman No. 269 Telp. (0265) 771204 Ciamis <br> 
+                    Email: <a href="mailto:surat@smkn1cms.net?subject:Informasi">surat@smkn1cms.net</a><br>
+                  Website: <a href="https://smkn1ciamis.id/">www.smkn1-cms.sch.id</a>
+                </span>
+            </td>
+        </tr>
+    </table>
+    <hr class="line-title">
+    </div> -->
+    <div class="col-md-12 mt-3">
+        <h3 align="center" class="text-secondary"> Para Pemilih Calon</h3>
+    </div>
+    <div class="box-body col-md-12">
+        <table id="example1" class="table table-bordered table-striped">
+            <thead> 
+                <tr>
+                    <th>No</th>
+                    <th>Nama User</th>
+                    <th>Nama Kandidat</th>
+                    <th>Created</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1;
+                foreach($rows as $row) : 
+                ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $row->nama_user ?></td>
+                    <td><?= $row->nama_kandidat ?></td>
+                    <td><?= $row->created ?></td>
+                </tr>
+            <?php endforeach; ?>
+            <script src="<?= base_url('assets/'); ?>js/jquery-3.5.1.min.js"></script>
+            <script src="<?= base_url('assets/'); ?>js/popper.min.js"></script>
+            <script src="<?= base_url('assets/'); ?>js/bootstrap.min.js"></script>
+            </tbody>
+            </table>
+            <script type="text/javascript">
+                window.print();
+            </script>
+    </div>
+</body>
+</html>
--- application/views/admin/suara.php
+++ application/views/admin/suara.php
@@ -3,18 +3,20 @@
         <div class="box-header">
             <h3 class="box-title"><?= $title ?></h3>
             <?= $this->session->flashdata('message'); ?> 
-            <?php sleep(1); unset($_SESSION['message']); ?>       
+            <?php sleep(1); unset($_SESSION['message']); ?> 
+            <a class="btn-sm btn-danger" href="<?= base_url('admin/suara/print'); ?>"><i class="fa fa-print"> Print</i></a>
+            <!-- <a class="btn-sm btn-success" href="<?= base_url('admin/suara/excel'); ?>"><i class="fa fa-download"> Excel</i></a>    -->
         </div>
         <!-- /.box-header -->
         <div class="box-body">
             <table id="example1" class="table table-bordered table-striped">
                 <thead>
                     <tr>
-                        <th>#</th>
+                        <th>NO</th>
                         <th>Nama User</th>
                         <th>Nama Kandidat</th>
                         <th>Created</th>
-                        <th></th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,12 +30,12 @@
                 <td><?= $row->created ?></td>
                 <td>
                     <a href="<?= site_url('admin/suara/hapus/' . $row->id_suara); ?>" 
-                    class="btn btn-sm btn-danger" onclick="return confirm ('Yakin Hapus?') ">
+                    class="btn btn-sm btn-danger" onclick="return confirm ('Yakin Hapus?')">
                     <i class="fa fa-trash-o"></i></a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-         </tbody>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+            </tbody>
             </table>
         </div>
     </div>
